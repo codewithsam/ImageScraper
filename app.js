5ad71bd946969aec74708b13c7d4070cf4c99049
samcodex <codewithsam@outlook.com>
2017-03-26 03:53:22 +0530

    changes for heroku

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 let ImageDB = require('./schema/ImageSchema');
 mongoose.connect('mongodb://codewithsam:qazwsxedc@ds141490.mlab.com:41490/imagescraper');
 
-
+app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -22,4 +22,4 @@ app.set('view engine', 'ejs');
 
 
 
-http.createServer(app).listen(2000);
+http.createServer(app).listen(app.get('port'));
